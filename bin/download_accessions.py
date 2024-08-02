571a8027bea117f5afc10812dffa9711ac364fe6
Nicholas Ellaby <nicholas.ellaby@ukhsa.gov.uk>
2024-08-02 15:45:43 +0100

    dev/added ID to efetch

--- bin/download_accessions.py
+++ bin/download_accessions.py
@@ -5,7 +5,7 @@ from Bio import Entrez
 import sys
 
 fasta_handle = Entrez.efetch(
-        db="nucleotide", id=gi, rettype="fasta", retmode="text"
+        db="nucleotide", id=sys.argv[1], rettype="fasta", retmode="text"
     )
 
 with open(f"{sys.argv[1]}_genomic.fna", "wt") as fh:
