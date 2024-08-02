7ff7c9583f45f14b4e6eeb7a007410f62fa3c24e
Nicholas Ellaby <nicholas.ellaby@ukhsa.gov.uk>
2024-08-02 17:07:53 +0100

    dev/ edited script to add wait time and error handling

--- bin/download_accessions.py
+++ bin/download_accessions.py
@@ -2,25 +2,38 @@
 
 from Bio import Entrez, SeqIO
 import sys
+import os
+import time
 
 def download_fasta(accession, email):
     Entrez.email = email  # Always tell NCBI who you are
-    print(Entrez.email)
-    handle = Entrez.efetch(db="nucleotide", id=accession, rettype="fasta", retmode="text")
-    print(handle)
-    # fasta_record = handle.read()
-    # handle.close()
+    try:
+        handle = Entrez.efetch(db="nucleotide", id=accession, rettype="fasta", retmode="text")
+        fasta_record = handle.read()
+        handle.close()
 
-    # # Save the FASTA record to a file
-    # with open(f"{accession}.fasta", "w") as file:
-    #     file.write(fasta_record)
+        # Define the file name
+        file_name = f"{accession}.fasta"
 
-    # print(f"FASTA file for accession {accession} has been saved as {accession}.fasta")
+        # Save the FASTA record to a file
+        with open(file_name, "w") as file:
+            file.write(fasta_record)
+        
+        # Check if the file is created
+        while not os.path.exists(file_name):
+            print(f"Waiting for {file_name} to be created...")
+            time.sleep(1)
 
-# Example usage
-print(sys.argv[1])
-print(sys.argv[2])
-download_fasta(sys.argv[1], sys.argv[2])
+        print(f"FASTA file for accession {accession} has been saved as {file_name}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python download_accessions.py <accession> <email>")
+    else:
+        download_fasta(sys.argv[1], sys.argv[2])
 
 
 # from Bio import Entrez
